022b017014701128893a75b95c5c8a72c48d6594
AhsanAAR <39828020+AhsanAAR@users.noreply.github.com>
2021-02-09 04:10:23 +0500

    commit

---
+++ GOMOKU_19001/Form1_19001.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace GOMOKU_19001
+{
+    partial class GameWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameWindow));
+            this.winCountBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dim12 = new System.Windows.Forms.RadioButton();
+            this.dim8 = new System.Windows.Forms.RadioButton();
+            this.dim10 = new System.Windows.Forms.RadioButton();
+            this.dim5 = new System.Windows.Forms.RadioButton();
+            this.dim3 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.gameGrid = new System.Windows.Forms.FlowLayoutPanel();
+            this.TwoPlayer = new System.Windows.Forms.CheckBox();
+            this.HintButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // winCountBox
+            // 
+            this.winCountBox.Location = new System.Drawing.Point(12, 91);
+            this.winCountBox.Name = "winCountBox";
+            this.winCountBox.Size = new System.Drawing.Size(82, 20);
+            this.winCountBox.TabIndex = 0;
+            this.winCountBox.TabStop = false;
+            this.winCountBox.Text = "5";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Moon 2.0 Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(12, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "WIN COUNT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Moon 2.0 Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(117, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(312, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "WELCOME TO GOMOKU";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dim12);
+            this.panel1.Controls.Add(this.dim8);
+            this.panel1.Controls.Add(this.dim10);
+            this.panel1.Controls.Add(this.dim5);
+            this.panel1.Controls.Add(this.dim3);
+            this.panel1.Location = new System.Drawing.Point(138, 89);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(344, 27);
+            this.panel1.TabIndex = 5;
+            // 
+            // dim12
+            // 
+            this.dim12.AutoSize = true;
+            this.dim12.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dim12.ForeColor = System.Drawing.Color.Black;
+            this.dim12.Location = new System.Drawing.Point(263, 2);
+            this.dim12.Name = "dim12";
+            this.dim12.Size = new System.Drawing.Size(70, 22);
+            this.dim12.TabIndex = 5;
+            this.dim12.Text = "12x12";
+            this.dim12.UseVisualStyleBackColor = true;
+            // 
+            // dim8
+            // 
+            this.dim8.AutoSize = true;
+            this.dim8.Checked = true;
+            this.dim8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dim8.ForeColor = System.Drawing.Color.Black;
+            this.dim8.Location = new System.Drawing.Point(129, 2);
+            this.dim8.Name = "dim8";
+            this.dim8.Size = new System.Drawing.Size(52, 22);
+            this.dim8.TabIndex = 3;
+            this.dim8.TabStop = true;
+            this.dim8.Text = "8x8";
+            this.dim8.UseVisualStyleBackColor = true;
+            // 
+            // dim10
+            // 
+            this.dim10.AutoSize = true;
+            this.dim10.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dim10.ForeColor = System.Drawing.Color.Black;
+            this.dim10.Location = new System.Drawing.Point(187, 2);
+            this.dim10.Name = "dim10";
+            this.dim10.Size = new System.Drawing.Size(70, 22);
+            this.dim10.TabIndex = 4;
+            this.dim10.Text = "10x10";
+            this.dim10.UseVisualStyleBackColor = true;
+            // 
+            // dim5
+            // 
+            this.dim5.AutoSize = true;
+            this.dim5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dim5.ForeColor = System.Drawing.Color.Black;
+            this.dim5.Location = new System.Drawing.Point(70, 3);
+            this.dim5.Name = "dim5";
+            this.dim5.Size = new System.Drawing.Size(52, 22);
+            this.dim5.TabIndex = 2;
+            this.dim5.Text = "5x5";
+            this.dim5.UseVisualStyleBackColor = true;
+            // 
+            // dim3
+            // 
+            this.dim3.AutoSize = true;
+            this.dim3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dim3.ForeColor = System.Drawing.Color.Black;
+            this.dim3.Location = new System.Drawing.Point(3, 3);
+            this.dim3.Name = "dim3";
+            this.dim3.Size = new System.Drawing.Size(52, 22);
+            this.dim3.TabIndex = 1;
+            this.dim3.Text = "3x3";
+            this.dim3.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Moon 2.0 Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(138, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 19);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "GRID SIZE";
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.ForeColor = System.Drawing.Color.Black;
+            this.startButton.Location = new System.Drawing.Point(12, 117);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(108, 23);
+            this.startButton.TabIndex = 8;
+            this.startButton.TabStop = false;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // gameGrid
+            // 
+            this.gameGrid.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.gameGrid.Location = new System.Drawing.Point(13, 147);
+            this.gameGrid.Name = "gameGrid";
+            this.gameGrid.Size = new System.Drawing.Size(500, 500);
+            this.gameGrid.TabIndex = 9;
+            // 
+            // TwoPlayer
+            // 
+            this.TwoPlayer.AutoSize = true;
+            this.TwoPlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TwoPlayer.ForeColor = System.Drawing.Color.Black;
+            this.TwoPlayer.Location = new System.Drawing.Point(138, 119);
+            this.TwoPlayer.Name = "TwoPlayer";
+            this.TwoPlayer.Size = new System.Drawing.Size(105, 20);
+            this.TwoPlayer.TabIndex = 10;
+            this.TwoPlayer.Text = "Two Player";
+            this.TwoPlayer.UseVisualStyleBackColor = true;
+            // 
+            // HintButton
+            // 
+            this.HintButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HintButton.ForeColor = System.Drawing.Color.Black;
+            this.HintButton.Location = new System.Drawing.Point(407, 120);
+            this.HintButton.Name = "HintButton";
+            this.HintButton.Size = new System.Drawing.Size(75, 23);
+            this.HintButton.TabIndex = 11;
+            this.HintButton.Text = "HINT";
+            this.HintButton.UseVisualStyleBackColor = true;
+            this.HintButton.Click += new System.EventHandler(this.HintButton_Click);
+            // 
+            // GameWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(525, 657);
+            this.Controls.Add(this.HintButton);
+            this.Controls.Add(this.TwoPlayer);
+            this.Controls.Add(this.gameGrid);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.winCountBox);
+            this.ForeColor = System.Drawing.Color.DimGray;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "GameWindow";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "GOMOKU";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox winCountBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton dim12;
+        private System.Windows.Forms.RadioButton dim8;
+        private System.Windows.Forms.RadioButton dim10;
+        private System.Windows.Forms.RadioButton dim5;
+        private System.Windows.Forms.RadioButton dim3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.FlowLayoutPanel gameGrid;
+        private System.Windows.Forms.CheckBox TwoPlayer;
+        private System.Windows.Forms.Button HintButton;
+    }
+}
+
---
+++ GOMOKU_19001/Form1_19001.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace GOMOKU_19001
+{
+    public partial class GameWindow : Form
+    {
+
+        Color Turn = Color.Red;
+        int TurnCount = 0;
+        bool computer;
+        void changeTurn()
+        {
+            if (Turn == Color.Red)
+                Turn = Color.Blue;
+            else
+                Turn = Color.Red;
+        }
+
+        Cell[,] grid;
+
+        class Cell : Button{
+            public Cell(int x, int y)
+            {
+                xpos = x;
+                ypos = y;
+            }
+            public int xpos, ypos;
+        }
+
+        void LoadGrid()
+        {
+            gameGrid.Controls.Clear();
+            grid = new Cell[dimension,dimension];
+            Cell myCell;
+            for(int ri = 0; ri < dimension; ri++)
+            {
+                for(int ci = 0; ci < dimension; ci++)
+                {
+                    myCell = new Cell(ci, ri);
+                    myCell.Width = gameGrid.Width / dimension;
+                    myCell.Height = gameGrid.Height/ dimension;
+                    myCell.Margin = new Padding(0, 0, 0, 0);
+                    myCell.BackColor = Color.Gray;
+                    myCell.Click += new EventHandler(this.cell_Click);
+                    gameGrid.Controls.Add(myCell);
+                    grid[myCell.ypos, myCell.xpos] = myCell;
+                }
+            }
+        }
+
+        bool win(Cell thisCell, Color checkColor, int n = 0)
+        {
+            int winCount = this.winCount;
+            winCount-=n;
+            int dy, dx;
+            int x, y;
+            int count = 1;
+            x = thisCell.xpos;
+            y = thisCell.ypos;
+            dy = -1; dx = 1;
+            x += dx; y += dy;
+            while (x >= 0 && x < dimension && y >= 0 
+                & y < dimension && grid[y,x].BackColor == checkColor)
+            {
+                x += dx; y += dy;
+                count++;
+            }
+            x = thisCell.xpos;
+            y = thisCell.ypos;
+            dy *= -1; dx *= -1;
+            x += dx; y += dy;
+            while (x >= 0 && x < dimension && y >= 0
+                & y < dimension && grid[y, x].BackColor == checkColor)
+            {
+                x += dx; y += dy;
+                count++;
+            }
+            if (count >= winCount)
+                return true;
+
+            count = 1;
+            x = thisCell.xpos;
+            y = thisCell.ypos;
+            dy = 0; dx = 1;
+            x += dx; y += dy;
+            while (x >= 0 && x < dimension && y >= 0
+                & y < dimension && grid[y, x].BackColor == checkColor)
+            {
+                x += dx; y += dy;
+                count++;
+            }
+            x = thisCell.xpos;
+            y = thisCell.ypos;
+            dy *= -1; dx *= -1;
+            x += dx; y += dy;
+            while (x >= 0 && x < dimension && y >= 0
+                & y < dimension && grid[y, x].BackColor == checkColor)
+            {
+                x += dx; y += dy;
+                count++;
+            }
+            if (count >= winCount)
+                return true;
+
+            count = 1;
+            x = thisCell.xpos;
+            y = thisCell.ypos;
+            dy = 1; dx = 1;
+            x += dx; y += dy;
+            while (x >= 0 && x < dimension && y >= 0
+                & y < dimension && grid[y, x].BackColor == checkColor)
+            {
+                x += dx; y += dy;
+                count++;
+            }
+            x = thisCell.xpos;
+            y = thisCell.ypos;
+            dy *= -1; dx *= -1;
+            x += dx; y += dy;
+            while (x >= 0 && x < dimension && y >= 0
+                & y < dimension && grid[y, x].BackColor == checkColor)
+            {
+                x += dx; y += dy;
+                count++;
+            }
+            if (count >= winCount)
+                return true;
+
+            count = 1;
+            x = thisCell.xpos;
+            y = thisCell.ypos;
+            dy = 1; dx = 0;
+            x += dx; y += dy;
+            while (x >= 0 && x < dimension && y >= 0
+                & y < dimension && grid[y, x].BackColor == checkColor)
+            {
+                x += dx; y += dy;
+                count++;
+            }
+            x = thisCell.xpos;
+            y = thisCell.ypos;
+            dy *= -1; dx *= -1;
+            x += dx; y += dy;
+            while (x >= 0 && x < dimension && y >= 0
+                & y < dimension && grid[y, x].BackColor == checkColor)
+            {
+                x += dx; y += dy;
+                count++;
+            }
+            if (count >= winCount)
+                return true;
+
+            return false;
+        }
+
+        void cell_Click(object sender, EventArgs e)
+        {
+            Cell thisCell = (Cell)sender;
+            if (grid[thisCell.ypos, thisCell.xpos].BackColor == Color.Gray)
+            {
+                thisCell.BackColor = Turn;
+                TurnCount++;
+                if (win(thisCell, Turn))
+                {
+                    if(computer)
+                        MessageBox.Show("you have won");
+                    else
+                        MessageBox.Show(Turn + " has won");
+                    TurnCount = 0;
+                    blueHints = 0;
+                    redHints = 0;
+                    gameGrid.Controls.Clear();
+                    return;
+                }
+                changeTurn();
+                if (TurnCount == dimension * dimension)
+                {
+                    MessageBox.Show("Game is DRAW!");
+                    TurnCount = 0;
+                    blueHints = 0;
+                    redHints = 0;
+                    gameGrid.Controls.Clear();
+                    return;
+                }
+            }
+            else
+            {
+                return;
+            }
+            if (computer)
+            {
+                for(int i = 0; i < winCount; i++)
+                {
+                    for (int ri = 0; ri < dimension; ri++)
+                    {
+                        for (int ci = 0; ci < dimension; ci++)
+                        {
+                            if (grid[ri, ci].BackColor == Color.Gray
+                                && win(grid[ri, ci], Turn, i))
+                            {
+                                grid[ri, ci].BackColor = Turn;
+                                if (i == 0)
+                                {
+                                    MessageBox.Show("Computer has won");
+                                    TurnCount = 0;
+                                    blueHints = 0;
+                                    redHints = 0;
+                                    gameGrid.Controls.Clear();
+                                    return;
+                                }
+                                TurnCount++;
+                                if (TurnCount == dimension * dimension)
+                                {
+                                    MessageBox.Show("Game is DRAW!");
+                                    TurnCount = 0;
+                                    blueHints = 0;
+                                    redHints = 0;
+                                    gameGrid.Controls.Clear();
+                                }
+                                changeTurn();
+                                return;
+                            }
+                        }
+                    }
+                    changeTurn();
+                    for (int ri = 0; ri < dimension; ri++)
+                    {
+                        for (int ci = 0; ci < dimension; ci++)
+                        {
+                            if (grid[ri, ci].BackColor == Color.Gray
+                                && win(grid[ri, ci], Turn, i))
+                            {
+                                changeTurn();
+                                grid[ri, ci].BackColor = Turn;
+                                TurnCount++;
+                                if (TurnCount == dimension * dimension)
+                                {
+                                    MessageBox.Show("Game is DRAW!");
+                                    TurnCount = 0;
+                                    blueHints = 0;
+                                    redHints = 0;
+                                    gameGrid.Controls.Clear();
+                                }
+                                changeTurn();
+                                return;
+                            }
+                        }
+                    }
+                    changeTurn();
+                }
+            }
+        }
+            
+        int winCount = 0, dimension = 0;
+
+        public GameWindow()
+        {
+            InitializeComponent();
+        }
+
+        int blueHints = 0, redHints = 0;
+        private void HintButton_Click(object sender, EventArgs e)
+        {
+            if (Turn == Color.Blue)
+            {
+                if (blueHints == 3)
+                    return;
+                blueHints++;
+            }
+            else
+            {
+                if (redHints == 3)
+                    return;
+                redHints++;
+            }
+            for (int i = 0; i < winCount; i++)
+            {
+                for (int ri = 0; ri < dimension; ri++)
+                {
+                    for (int ci = 0; ci < dimension; ci++)
+                    {
+                        if (grid[ri, ci].BackColor == Color.Gray
+                            && win(grid[ri, ci], Turn, i))
+                        {
+                            grid[ri, ci].BackColor = Color.Yellow;
+                            MessageBox.Show("Your Hint");
+                            grid[ri, ci].BackColor = Color.Gray;
+                            return;
+                        }
+                    }
+                }
+                changeTurn();
+                for (int ri = 0; ri < dimension; ri++)
+                {
+                    for (int ci = 0; ci < dimension; ci++)
+                    {
+                        if (grid[ri, ci].BackColor == Color.Gray
+                            && win(grid[ri, ci], Turn, i))
+                        {
+                            grid[ri, ci].BackColor = Color.Yellow;
+                            MessageBox.Show("Your Hint");
+                            grid[ri, ci].BackColor = Color.Gray;
+                            changeTurn();
+                            return;
+                        }
+                    }
+                }
+                changeTurn();
+            }
+        }
+
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            TurnCount = 0;
+            dimension = 0;
+            blueHints = 0;
+            redHints = 0;
+            if (dim3.Checked)
+                dimension = 3;
+            else if (dim5.Checked)
+                dimension = 5;
+            else if (dim8.Checked)
+                dimension = 8;
+            else if (dim10.Checked)
+                dimension = 10;
+            else if (dim12.Checked)
+                dimension = 12;
+            else
+            {
+                MessageBox.Show("Select a Grid Size");
+                return;    
+            }
+            if(winCountBox.Text == "" ||
+                (winCount = Int32.Parse(winCountBox.Text)) <= 2 || winCount > dimension)
+            {
+                MessageBox.Show("Incorrect Wincount");
+                return;
+            }
+            computer = !TwoPlayer.Checked;
+            LoadGrid();
+        }
+    }
+}
